efcbec8930b26e8fd6862509218a7ba43cfa75f4
Nigel Gibbs <nigel@gibbsoft.com>
2019-06-24 12:16:54 +0100

    Run MySQL as non-root user
    This is for tigheter security and compatibility with OpenShift.

--- mysql/install.sh
+++ mysql/install.sh
@@ -5,4 +5,5 @@ echo "*** Installing MySQL"
 apk add --update --no-cache mysql curl
 mysql_install_db --user=mysql --basedir=/usr/ --datadir=/mysql/data --force
 mkdir -p /run/mysqld/
-chown -R mysql /mysql /run/mysqld/
+chown -R mysql:root /mysql /run/mysqld/
+chmod -R 775 /mysql /run/mysqld/
--- mysql/run.sh
+++ mysql/run.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-/usr/bin/mysqld_safe --user=mysql --basedir=/usr/ --datadir=/mysql/data
+
+if ! whoami &> /dev/null; then
+  if [ -w /etc/passwd ]; then
+    sed -i "s/:x:mysql:/:x:$(id -u):/" /etc/passwd
+  fi
+fi
+
+/usr/bin/mysqld_safe --user=mysql --basedir=/usr/ --datadir=/mysql/data --syslog
